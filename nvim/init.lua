b51d0058ca72cc6390e26de9f483f78857151172
Matheus Henrique <matherique@gmail.com>
2024-02-01 15:17:16 -0300

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -237,8 +237,8 @@ vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none", fg = "none" })
 -- vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none", fg = "none" })
 
 -- some change in themes
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#3B4252", fg = "#5E81AC" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3B4252" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
